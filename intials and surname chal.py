61758e3da495908d283e16150a0036c0853f55d3
buggedf11 <120758948+buggedf11@users.noreply.github.com>
2023-10-18 12:14:47 +0000

    change intials and surname chal.py

--- intials and surname chal.py
+++ intials and surname chal.py
@@ -1,6 +1,6 @@
 name = input("enetr first name")
 sname = input ("enter surname")
 inital = name [0:1]
-upperin = inital.upper
-usname = sname.upper
+upperin = inital.upper()
+usname = sname.upper()
 print (upperin,usname)
