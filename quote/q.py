7c8903c593f291916d92bfc3d9998ee3c1f7cc69
buggedf11 <120758948+buggedf11@users.noreply.github.com>
2023-11-15 14:53:32 +0000

    rr

---
+++ quote/q.py
@@ -0,0 +1,9 @@
+import random
+
+with open("q.txt", "r") as file:
+    quotes = file.readlines()
+
+random_index = random.randint(0, len(quotes) - 1)
+random_quote = quotes[random_index].strip()
+
+print(random_quote)
